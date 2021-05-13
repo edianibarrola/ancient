df84f7dd77cf136867a2b4a1d90a2d9126e8dfef
edianibarrola <edianibarrola@gmail.com>
2021-05-13 23:40:21 +0000

    update

--- src/front/js/pages/site_info.js
+++ src/front/js/pages/site_info.js
@@ -8,44 +8,49 @@ export const SiteInfo = props => {
 	const params = useParams();
 
 	return (
-		<div className="row ">
-			<div className="col  text-center">
-				<h1>{store.site.name ? store.site.name : "name unavailable"} </h1>
-				{console.log(process.env.MAPAPIKEY)}
-				<div className="polaroid mx-auto no-gutters">
-					<iframe
-						width="300"
-						height="300"
-						style={{ border: "1px solid black" }}
-						loading="lazy"
-						allowFullScreen
-						// src={`https://www.google.com/maps/embed/v1/place?key=${
-						// 	process.env.MAPAPIKEY
-						// }&maptype=satellite&q=puma+punku`}
-						src={
-							store.site.map_link
-								? `https://www.google.com/maps/embed/v1/place?key=${
-										process.env.MAPAPIKEY
-								  }&maptype=satellite&q=${store.site.map_link}`
-								: `https://www.google.com/maps/embed/v1/place?key=${
-										process.env.MAPAPIKEY
-								  }&maptype=satellite&q=stone+henge+megalith`
-						}
-					/>
-					<h3>{store.site.name}</h3>
+		<>
+			<h1 className="text-center">{store.site.name ? store.site.name : "name unavailable"} </h1>
+			<div className="row d-flex justify-content-center">
+				<div className="col-4  text-center">
+					{console.log(process.env.MAPAPIKEY)}
+					<div className="polaroid mx-auto no-gutters">
+						<iframe
+							width="300"
+							height="300"
+							style={{ border: "1px solid black" }}
+							loading="lazy"
+							allowFullScreen
+							// src={`https://www.google.com/maps/embed/v1/place?key=${
+							// 	process.env.MAPAPIKEY
+							// }&maptype=satellite&q=puma+punku`}
+							src={
+								store.site.map_link
+									? `https://www.google.com/maps/embed/v1/place?key=${
+											process.env.MAPAPIKEY
+									  }&maptype=satellite&q=${store.site.map_link}`
+									: `https://www.google.com/maps/embed/v1/place?key=${
+											process.env.MAPAPIKEY
+									  }&maptype=satellite&q=stone+henge+megalith`
+							}
+						/>
+						<h3>{store.site.name}</h3>
 
-					<h5>
-						{store.site.country ? store.site.country : " "}{" "}
-						{store.site.continent ? store.site.continent : " "}
-					</h5>
-					<h5>
-						{store.site.date_range_end
-							? store.site.date_range_start + "    -    " + store.site.date_range_end
-							: store.site.date_range_start}
-					</h5>
+						<h5>
+							{store.site.country ? store.site.country : " "}{" "}
+							{store.site.continent ? store.site.continent : " "}
+						</h5>
+						<h5>
+							{store.site.date_range_end
+								? store.site.date_range_start + "    -    " + store.site.date_range_end
+								: store.site.date_range_start}
+						</h5>
+					</div>
+				</div>
+				<div className="col-4 infocol ">
+					<p>{store.site.details}</p>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
